898eeefbe18b4b094285357e471e51472dc09a53
WadimNikulin <77461580+WadimNikulin@users.noreply.github.com>
2021-02-01 21:40:24 +0300

    Delete more_4.py

--- more_4.py
+++
@@ -1,8 +0,0 @@
-n = float(input(" Введите целое положительное число: "))
-answer = 0
-while n != 0:
-    m = n % 10
-    if m > answer:
-        answer = m
-    n //= 10
-print("Самая большая цифра: ", int(answer))
